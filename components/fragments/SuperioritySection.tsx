9e486709f097f5ec4f97deef297a1bb1b07fceee
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-01-06 12:54:44 +0700

    umar: menambahkan struktur div untuk grid

--- components/fragments/SuperioritySection.tsx
+++ components/fragments/SuperioritySection.tsx
@@ -1,11 +1,20 @@
 export default function Superiority() {
-    return (
-      <section>
-        <div>
-          <h2>Kenapa Pilih Askha Jaya?</h2>
-          <p>Temukan kelezatan kami di sini dan rasakan sendiri kenikmatannya!</p>
-        </div>
-      </section>
-    )
-  }
-  
+  return (
+    <section>
+      <div>
+        <h2>Kenapa Pilih Askha Jaya?</h2>
+        <p>Temukan kelezatan kami di sini dan rasakan sendiri kenikmatannya!</p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* Card 1 */}
+        <div></div>
+        {/* Card 2 */}
+        <div></div>
+        {/* Card 3 */}
+        <div></div>
+      </div>
+
+    </section>
+  )
+}
