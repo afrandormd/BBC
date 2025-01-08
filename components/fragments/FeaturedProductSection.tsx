cbda654ca79c2796282473ca4847b453e2583712
TiasHaikal <tiashaikalmulyana@gmail.com>
2025-01-08 19:46:40 +0700

    Tias: Membuat Daftar Produk dengan Gambar yang Sesuai dan membuat folder product di folder public

--- components/fragments/FeaturedProductSection.tsx
+++ components/fragments/FeaturedProductSection.tsx
@@ -6,3 +6,26 @@ interface Product {
   price: number;
   image: string;
 }
+
+const products: Product[] = [
+  {
+    name: "Keripik Pisang Karamel Madu",
+    price: 27500,
+    image: "/product/keripik-pisang.jpg",
+  },
+  {
+    name: "Pisang Muli Oven",
+    price: 25000,
+    image: "/product/muli-ove.jpg",
+  },
+  {
+    name: "Keripik Sale Pisang",
+    price: 25000,
+    image: "/product/sale-pisang.jpg",
+  },
+  {
+    name: "Keripik Nangka Oven",
+    price: 27000,
+    image: "/product/chips-nangka.jpg",
+  },
+];
