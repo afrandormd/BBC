c487da410ce2f5be8e23b26994bb165786b08115
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2025-01-10 00:48:42 +0700

    rando: membuat objek data dummy untuk card produk

--- app/allproducts/page.tsx
+++ app/allproducts/page.tsx
@@ -9,3 +9,42 @@ interface Product {
   price: number;
   image: string;
 }
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: "Keripik Pisang Karamel Madu",
+    price: 27500,
+    image: "/product/keripik-pisang.jpg",
+  },
+  {
+    id: 2,
+    name: "Pisang Muli Oven",
+    price: 25000,
+    image: "/product/muli-ove.jpg",
+  },
+  {
+    id: 3,
+    name: "Keripik Sale Pisang",
+    price: 25000,
+    image: "/product/sale-pisang.jpg",
+  },
+  {
+    id: 4,
+    name: "Keripik Nangka Oven",
+    price: 27000,
+    image: "/product/chips-nangka.jpg",
+  },
+  {
+    id: 5,
+    name: "Keripik Nangka",
+    price: 27000,
+    image: "/product/sale-pisang.jpg",
+  },
+  {
+    id: 6,
+    name: "Kacang Udang",
+    price: 27000,
+    image: "/product/muli-ove.jpg",
+  },
+];
