e4bd32c738026698426ec4cbbc4515872e0cdbb2
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-01-10 11:09:43 +0700

    umar: membuat struktur dasar komponen Testimonial

---
+++ components/fragments/TestimonialSection.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import * as React from "react"
+
+export default function Testimonial() {
+  return (
+    <section className="w-full py-12 bg-[#FFF4D9]">
+      <div className="container px-4 mx-auto">
+        <h2 className="text-3xl font-bold text-secondary mb-2">
+          Apa Kata Pelanggan Kami
+        </h2>
+        <p className="text-secondary/80">
+          Review pelanggan mengenai layanan dan produk dari askha jaya
+        </p>
+      </div>
+    </section>
+  )
+}
