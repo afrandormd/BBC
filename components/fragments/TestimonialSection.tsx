8da60a90b8d57e35af81b51eaa947cabb8668ff7
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-01-10 14:05:35 +0700

    umar: menambahkan avatar pada setiap testimonial

--- components/fragments/TestimonialSection.tsx
+++ components/fragments/TestimonialSection.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { Card, CardContent } from "../elements/card"
+import { Avatar, AvatarFallback, AvatarImage } from "../elements/avatar"
 
 
 const testimonials = [
@@ -35,13 +36,21 @@ export default function Testimonial() {
                 </p>
 
                 <div>
-                    {testimonials.map((testimonial, index) => (
-                        <Card key={index} className="bg-white shadow-md mb-4">
-                            <CardContent className="p-4">
-                                <strong>{testimonial.name}</strong> ({testimonial.location}): {testimonial.quote}
-                            </CardContent>
-                        </Card>
-                    ))}
+                {
+  testimonials.map((testimonial, index) => (
+    <Card key={index} className="bg-white shadow-md mb-4">
+      <CardContent className="p-4 flex items-center gap-4">
+        <Avatar>
+          <AvatarImage src="/placeholder.svg" alt={testimonial.name} />
+          <AvatarFallback>{testimonial.name[0]}</AvatarFallback>
+        </Avatar>
+        <div>
+          <strong>{testimonial.name}</strong> ({testimonial.location}): {testimonial.quote}
+        </div>
+      </CardContent>
+    </Card>
+  ))
+}
                 </div>
 
             </div>
