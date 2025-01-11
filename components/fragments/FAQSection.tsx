6629675ab85a3cbc110dfe3dc08edd68f27a4c49
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2025-01-11 15:02:25 +0700

    rando: import package yang dibutuhkan di file FAQSection.tsx

---
+++ components/fragments/FAQSection.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Search } from "lucide-react";
+import { Accordion } from "../elements/accordion";
+// import {
+//   Accordion,
+//   AccordionContent,
+//   AccordionItem,
+//   AccordionTrigger,
+// } from "../elements/accordion"
+// import { Input } from "../elements/input"
+//
