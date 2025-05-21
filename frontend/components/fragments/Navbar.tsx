6ac01096b8ae25a7e515529dedd41fb682678d49
TiasHaikal <tiashaikalmulyana@gmail.com>
2025-05-21 10:20:23 +0700

    Haikal: Menambahkan toggler menu pada navbar

--- frontend/components/fragments/Navbar.tsx
+++ frontend/components/fragments/Navbar.tsx
@@ -57,6 +57,45 @@ const Navbar = () => {
                 </Button>
               </div>
             </div>
+            {/* Toggler Menu */}
+            <div className="flex items-center md:hidden">
+              <button
+                className="inline-flex items-center justify-center p-2 text-white rounded-md hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={toggleNavbar}
+              >
+                {isClick ? (
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
         )
