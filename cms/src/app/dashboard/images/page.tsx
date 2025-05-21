ffdaa9bc073d62b973a6e886c13d102734f14ea3
TirtaRizki <tirtarzk30@gmail.com>
2025-05-21 08:57:29 +0700

    Membuat file autentikasi image untuk memasukan produk  di dalam folder dashboard

---
+++ cms/src/app/dashboard/images/page.tsx
@@ -0,0 +1,6 @@
+
+import { ImageManagementClient } from '@/components/dashboard/images/ImageManagementClient';
+
+export default function ImageManagementPage() {
+  return <ImageManagementClient />;
+}
