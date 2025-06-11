97bd5e7d4167669796b3b94db1abe3bcfb5e7a82
afrandormd <afrandormd@gmail.com>
2025-06-11 12:49:34 +0700

    rando: membuat file core jwt.ts

---
+++ backend/lib/jwt.ts
@@ -0,0 +1,25 @@
+// lib/jwt.ts
+import jwt from 'jsonwebtoken';
+
+// buat konstanta JWT
+const JWT_KEY = "BBC";
+const JWT_EXPIRED = "10m"
+
+// buat interface
+export interface JwtInfo {
+    code: string;
+}
+
+// buat fungsi sign jwt
+export const jwtSign = (info: JwtInfo) => {
+    return jwt.sign(info, JWT_KEY!, { expiresIn: JWT_EXPIRED });
+}
+
+// buat fungsi verify jwt
+export const jwtVerify = (token: string): JwtInfo | null => {
+    try {
+        return jwt.verify(token, JWT_KEY) as JwtInfo;
+    } catch (err) {
+        return null;
+    }
+}
