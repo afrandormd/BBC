fe3c882b4d1bce3de30d7c14d794a47cf58ff338
TiasHaikal <tiashaikalmulyana@gmail.com>
2025-06-14 17:44:14 +0700

    Haikal: Membuat Section Testimonial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedProductSection from "@/components/fragments/FeaturedProductSection";
 import HeroSection from "@/components/fragments/HeroSection";
 import Superiority from "@/components/fragments/SuperioritySection";
+import Testimonial from "@/components/fragments/TestimonialSection";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <FeaturedProductSection />
       {/* Bagian Superiority Sectin  */}
       <Superiority />
+       {/* Bagian Testimonial Section */}
+      <Testimonial />
     </>
   );
 }
