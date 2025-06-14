24919f0b54e4202fb98e4952fd780e63a7c23db7
TiasHaikal <tiashaikalmulyana@gmail.com>
2025-06-14 20:08:21 +0700

    Haikal: Membuat About Section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedProductSection from "@/components/fragments/FeaturedProductSectio
 import HeroSection from "@/components/fragments/HeroSection";
 import Superiority from "@/components/fragments/SuperioritySection";
 import Testimonial from "@/components/fragments/TestimonialSection";
+import AboutSection from "@/components/fragments/AboutSection";
 
 export default function Home() {
   return (
@@ -12,8 +13,10 @@ export default function Home() {
       <FeaturedProductSection />
       {/* Bagian Superiority Sectin  */}
       <Superiority />
-       {/* Bagian Testimonial Section */}
+      {/* Bagian Testimonial Section */}
       <Testimonial />
+      {/* Bagian About Section */}
+      <AboutSection />
     </>
   );
 }
